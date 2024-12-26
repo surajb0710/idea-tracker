5e4a4affcc0c5bbdbaf55269e184f4813ae9ca63
Suraj <104007414+surajb0710@users.noreply.github.com>
2024-12-26 11:57:10 +0530

    Env variables issue fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import replace from '@rollup/plugin-replace';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    replace({
+      preventAssignment: true,
+      values: {
+        'import.meta.env.VITE_APPWRITE_ENDPOINT': JSON.stringify(
+          process.env.VITE_APPWRITE_ENDPOINT
+        ),
+      },
+    }),
+    replace({
+      preventAssignment: true,
+      values: {
+        'import.meta.env.VITE_APPWRITE_PROJECT_ID': JSON.stringify(
+          process.env.VITE_APPWRITE_PROJECT_ID
+        ),
+      },
+    }),
+  ],
+});
