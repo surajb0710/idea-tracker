c1e65370adc02e14dcd026b9679df98ee87f03e0
Suraj <104007414+surajb0710@users.noreply.github.com>
2024-12-26 11:17:56 +0530

    appwrite file updated

--- src/appwrite.js
+++ src/appwrite.js
@@ -2,8 +2,8 @@ import { Client, Databases, Account } from 'appwrite';
 
 const client = new Client();
 client
-  .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('676c02860025f2c22e61'); // Replace with your project ID
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // Replace with your project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
